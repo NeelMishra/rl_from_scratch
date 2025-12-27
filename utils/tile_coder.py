2ba357668d5ebb8948f4e8c6ca99b65c216061d6
NeelMishra <Mishraneel99@gmail.com>
2025-12-27 23:07:29 +0530

    Added a Tile Coding, Richard Sutton Implementation

--- utils/tile_coder.py
+++ utils/tile_coder.py
@@ -1,10 +1,10 @@
 import numpy as np
-
+import math
 class IHT:
 
     def __init__(self, size):
 
-        if type(size) != int or size <= 0:
+        if not isinstance(size, int) or size <= 0:
             raise ValueError("Size must be integer and > 0")
 
         self.size = size
@@ -44,3 +44,42 @@ class TileCoder:
 
         self.iht = IHT(hash_size)
         self.scale = self.tiles_per_dim/(self.state_high - self.state_low)
+
+    def _scale_state(self, state):
+
+        state = np.array(state, dtype=float)
+        scaled = (state - self.state_low) * self.scale
+
+        return scaled
+    
+    def get_active_tiles(self, state, action):
+
+        if not (0 <= action < self.num_actions and isinstance(action, (int, np.integer)) ):
+            raise ValueError(f"Action must be between [0 to the {self.num_actions}) and must be of type int")
+
+        scaled_state = self._scale_state(state)
+
+        active_tiles = self._tiles(
+                                  floats = scaled_state,
+                                  ints=(action, ),
+                                  readonly=False
+                                  )
+
+        return active_tiles
+    
+    def _tiles(self, floats, ints, readonly=False):
+
+        q_floats = [math.floor(curr_float * self.num_tilings) for curr_float in floats]
+        feature_idxs = []
+        for tiling in range(self.num_tilings):
+            
+            b = tiling
+            coords = [b]
+            for q in q_floats:
+                curr_bin = (q+b) // self.num_tilings
+                coords.append(curr_bin)
+                b += 2 * tiling
+            coords.extend(ints)
+            feature_idxs.append(self.iht.get_index(coords, readonly=readonly))
+        return feature_idxs
+            
